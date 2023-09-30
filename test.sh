59427ce67153cd369223f00ee95cad3928362ce4
andrewp-as-is <andrewp-as-is@users.noreply.github.com>
2023-09-30 13:12:57 +0300

    -

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-{ set +x; } 2>/dev/null
-
-find -H ~/git -type d -mindepth 1 -maxdepth 1 | while read path; do
-    cd "$path" || exit
-     ( set -x; cp -R ~/git/github-metadata/repos/"${PWD##*/}"/ .github-metadata )
-done
